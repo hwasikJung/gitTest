2015c2694447cc94956ad0b0eb36cd56e10898f7
hs.jung <hs.jung@sqisoft.com>
2023-02-11 00:18:47 +0900

    init4

--- vue-cli/vue-git-test/src/App.vue
+++ vue-cli/vue-git-test/src/App.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     testfdgfdg`asdasdjnbvjhgjhgjhghj
-asdasdiuhiuhukhuk
+dfsfdsdsffdfsfdsfsd
   </div>
 </template>
 
